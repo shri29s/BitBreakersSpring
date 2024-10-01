d029d0935209da5663f6fc9605b5a6f48a0d1462
shri29s <shricharan.r24@iiits.in>
2024-10-01 18:47:27 +0530

    Created backend template

---
+++ bitbreaker/src/main/java/com/bitbreakers/bitbreaker/BitbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.bitbreakers.bitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbreakerApplication.class, args);
+	}
+
+}
---
+++ bitbreaker/src/test/java/com/bitbreakers/bitbreaker/BitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbreakers.bitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
