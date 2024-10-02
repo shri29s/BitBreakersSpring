83ebd620a7501ffd9cb3ce35e5b530787455ccc6
shri29s <shricharan.r24@iiits.in>
2024-10-02 10:12:49 +0530

    commit

--- bitbreaker/src/main/java/com/bitbreakers/bitbreaker/controller/HomeController.java
+++ bitbreaker/src/main/java/com/bitbreakers/bitbreaker/controller/HomeController.java
@@ -1,13 +1,35 @@
 package com.bitbreakers.bitbreaker.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bitbreakers.bitbreaker.Startup;
+import com.bitbreakers.bitbreaker.model.StartupModel;
+import com.bitbreakers.bitbreaker.service.StartupService;
+
 
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
+    StartupService startupService;
+    public HomeController(StartupService startupService){
+        this.startupService = startupService;
+    }
+
     @GetMapping("/")
     public String homes() {
         return "Success";
+    }
+
+    @PostMapping("/addStartup")
+    public ResponseEntity<String> addStartup(@RequestBody StartupModel startup) {
+        this.startupService.addStartup(startup);
+        return new ResponseEntity<>("Saved", HttpStatus.OK);
     }
 }
--- bitbreaker/src/main/java/com/bitbreakers/bitbreaker/service/StartupService.java
+++ bitbreaker/src/main/java/com/bitbreakers/bitbreaker/service/StartupService.java
@@ -2,10 +2,17 @@ package com.bitbreakers.bitbreaker.service;
 
 import org.springframework.stereotype.Service;
 
+import com.bitbreakers.bitbreaker.model.StartupModel;
+import com.bitbreakers.bitbreaker.repo.StartupRepo;
+
 @Service
 public class StartupService {
-    // Startup startupModel;
-    // public StartupService(Startup startup){
-    //     this.startupModel = startup;
-    // }
+    StartupRepo startupRepo;
+    public StartupService(StartupRepo startupRepo){
+        this.startupRepo = startupRepo;
+    }
+
+    public void addStartup(StartupModel startup){
+        this.startupRepo.save(startup);
+    }
 }
